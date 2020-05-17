967f80af3c6aa51b99c5599197ee8b239f8bf55f
cankot97 <63225267+cankot97@users.noreply.github.com>
2020-05-17 15:16:34 +1000

    Add files via upload

---
+++ 53.py
@@ -0,0 +1,108 @@
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QLineEdit
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtCore import QSize
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode (GPIO.BOARD)
+GPIO.setup(10,GPIO.OUT)
+
+CODE = {' ': ' ',
+        "'": '.----.',
+        '(': '-.--.-',
+        ')': '-.--.-',
+        ',': '--..--',
+        '-': '-....-',
+        '.': '.-.-.-',
+        '/': '-..-.',
+        '0': '-----',
+        '1': '.----',
+        '2': '..---',
+        '3': '...--',
+        '4': '....-',
+        '5': '.....',
+        '6': '-....',
+        '7': '--...',
+        '8': '---..',
+        '9': '----.',
+        ':': '---...',
+        ';': '-.-.-.',
+        '?': '..--..',
+        'A': '.-',
+        'B': '-...',
+        'C': '-.-.',
+        'D': '-..',
+        'E': '.',
+        'F': '..-.',
+        'G': '--.',
+        'H': '....',
+        'I': '..',
+        'J': '.---',
+        'K': '-.-',
+        'L': '.-..',
+        'M': '--',
+        'N': '-.',
+        'O': '---',
+        'P': '.--.',
+        'Q': '--.-',
+        'R': '.-.',
+        'S': '...',
+        'T': '-',
+        'U': '..-',
+        'V': '...-',
+        'W': '.--',
+        'X': '-..-',
+        'Y': '-.--',
+        'Z': '--..',
+        '_': '..--.-'}
+
+def dot():
+        GPIO.output(10,GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(10,GPIO.LOW)
+        time.sleep(0.2)
+
+def dash():
+        GPIO.output(10,GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(10,GPIO.LOW)
+        time.sleep(0.2)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.setMinimumSize(QSize(320, 140))    
+        self.setWindowTitle("LED") 
+
+        self.nameLabel = QLabel(self)
+        self.nameLabel.setText('Name:')
+        self.line = QLineEdit(self)
+
+        self.line.move(80, 20)
+        self.line.resize(200, 32)
+        self.nameLabel.move(20, 20)
+
+        pybutton = QPushButton('OK', self)
+        pybutton.clicked.connect(self.clickMethod)
+        pybutton.resize(200,32)
+        pybutton.move(80, 60)        
+
+    def clickMethod(self):
+        print( self.line.text())
+        for letter in self.line.text():
+            for symbol in CODE[letter.upper()]:
+                if symbol == '-':
+                    dash()
+                elif symbol == '.':
+                    dot()
+                else:
+                    time.sleep(0.5)
+            time.sleep(0.5)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
